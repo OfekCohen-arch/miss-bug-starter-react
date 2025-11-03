4aeb753085dba2413d158a4b71b901f781d49311
Ofek Cohen <ofek074@gmail.com>
2025-11-03 14:41:42 +0200

    In the BugDetails page, show the bug's description

--- cmps/BugPreview.jsx
+++ cmps/BugPreview.jsx
@@ -2,6 +2,5 @@ export function BugPreview({bug}) {
     return <article className="bug-preview">
         <p className="title">{bug.title}</p>
         <p>Severity: <span>{bug.severity}</span></p>
-        <p>Description: <span>{bug.description}</span></p>
     </article>
 }
