c37ca32e9ee3f7b6ae9e87baf8a34131e647b50f
Ofek Cohen <ofek074@gmail.com>
2025-11-17 14:26:28 +0200

    Add userRoute

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import { bugService } from "./services/bug.service.js";
 import { loggerService } from "./services/logger.service.js";
-
+import { userService } from "./services/user.service.js";
+import { authService } from "./services/auth.service.js";
 
 const app = express();
 app.use(express.static("public"));
@@ -12,35 +13,35 @@ app.use(express.json());
 app.set("query parser", "extended");
 
 app.get("/api/bug", (req, res) => {
-  const queryOptions = parseQueryParams(req.query)
+  const queryOptions = parseQueryParams(req.query);
   bugService.query(queryOptions).then((bugs) => res.send(bugs));
 });
 function parseQueryParams(queryParams) {
-    const filterBy = {
-        txt: queryParams.txt || '',
-        minSeverity: +queryParams.minSeverity || 0,
-        labels: queryParams.labels || [],
-        paginationOn: queryParams.paginationOn === 'true',
-        pageIdx : +queryParams.pageIdx || 0
-    }
+  const filterBy = {
+    txt: queryParams.txt || "",
+    minSeverity: +queryParams.minSeverity || 0,
+    labels: queryParams.labels || [],
+    paginationOn: queryParams.paginationOn === "true",
+    pageIdx: +queryParams.pageIdx || 0,
+  };
 
-    const sortBy = {
-        sortField: queryParams.sortField || '',
-        sortDir: +queryParams.sortDir || 1,
-    }
-    
+  const sortBy = {
+    sortField: queryParams.sortField || "",
+    sortDir: +queryParams.sortDir || 1,
+  };
 
-    return { filterBy, sortBy}
+  return { filterBy, sortBy };
 }
 app.get("/api/bug/:bugId", (req, res) => {
   const bugId = req.params.bugId;
   const visitedBugsIds = req.cookies.visitedBugsIds || [];
-  if(visitedBugsIds.includes(bugId)) visitedBugsIds.push(bugId)
-  if(visitedBugsIds.length>3) return res.status(401).send("Wait for a bit");
-  res.cookie('visitedBugsIds',visitedBugsIds,{maxAge: 7000})
-  bugService.getById(bugId)
+  if (visitedBugsIds.includes(bugId)) visitedBugsIds.push(bugId);
+  if (visitedBugsIds.length > 3) return res.status(401).send("Wait for a bit");
+  res.cookie("visitedBugsIds", visitedBugsIds, { maxAge: 7000 });
+  bugService
+    .getById(bugId)
     .then((bug) => {
-      res.send(bug)
+      res.send(bug);
     })
     .catch((err) => {
       loggerService.error(err);
@@ -54,25 +55,63 @@ app.put("/api/bug/:id", (req, res) => {
   bugService.save(bug).then((bug) => res.send(bug));
 });
 app.post("/api/bug", (req, res) => {
-  const {title, description, severity, createdAt } = req.body;
-  const bug = {title, description, severity, createdAt };
-  bugService.save(bug).then((bug) => res.send(bug));
-});
+  const { title, description, severity, createdAt } = req.body;
+  const loggedInUser = authService.validateToken(req.cookies.loginToken)
 
+  if(!loggedInUser) return res.status(401).send('Cannot add car')
+
+  const bug = { title, description, severity, createdAt};
+  bugService.save(bug,loggedInUser).then((bug) => res.send(bug));
+});
 
 app.delete("/api/bug/:bugId", (req, res) => {
   const bugId = req.params.bugId;
-  bugService.remove(bugId)
-  .then(res.send("OK"))
-  .catch(err => {
-			loggerService.error(err)
-			res.status(404).send(err)
-		})
+  const loggedInUser = authService.validateToken(req.cookies.loginToken)
+  bugService
+    .remove(bugId,loggedInUser)
+    .then(res.send("OK"))
+    .catch((err) => {
+      loggerService.error(err);
+      res.status(404).send(err);
+    });
 });
 
-app.get('/*all', (req, res) => {
-	res.sendFile(path.resolve('public/index.html'))
+
+app.post('/api/auth/login', (req, res) => {
+    const { username, password } = req.body
+    authService.checkLogin({ username, password })
+        .then(user => {
+            const loginToken = authService.getLoginToken(user)
+            res.cookie('loginToken', loginToken)
+            res.send(user)
+        })
+        .catch(err => {
+            loggerService.error('Cannot signup', err)
+            res.status(404).send('Invalid Credentials')
+        })
 })
+app.post("/api/auth/signup", (req, res) => {
+  const { userName, fullName, password } = req.body;
+  userService
+    .add({ userName, fullName, password })
+    .then((user) => {
+      const loginToken = authService.getLoginToken(user);
+      res.cookie("loginToken", loginToken);
+      res.send(user);
+    })
+    .catch((err) => {
+      loggerService.error("Cannot signup", err);
+      res.status(400).send("Cannot signup");
+    });
+});
+app.post('/api/auth/logout', (req, res) => {
+    res.clearCookie('loginToken')
+    res.send('logged-out!')
+})
+
+app.get("/*all", (req, res) => {
+  res.sendFile(path.resolve("public/index.html"));
+});
 
 const port = 3030;
 app.listen(port);
--- services/bug.service.js
+++ services/bug.service.js
@@ -27,10 +27,10 @@ function query({ filterBy, sortBy }) {
     );
   }
   if (filterBy.labels && filterBy.labels.length > 0) {
-        filteredBugs = 
-            filteredBugs.filter(bug => 
-                filterBy.labels.some(label => bug?.labels?.includes(label)))
-    }
+    filteredBugs = filteredBugs.filter((bug) =>
+      filterBy.labels.some((label) => bug?.labels?.includes(label))
+    );
+  }
   if (sortBy.sortField === "severity" || sortBy.sortField === "createdAt") {
     const { sortField } = sortBy;
 
@@ -56,23 +56,31 @@ function getById(bugId) {
   return Promise.resolve(bug);
 }
 
-function remove(bugId) {
+function remove(bugId,loggedInUser) {
   const idx = bugs.findIndex((bug) => bug._id === bugId);
 
   if (idx === -1) return Promise.reject("Bug not found");
-
+  if (!loggedInUser.isAdmin && bugs[idx].creator._id !== loggedInUser._id) {
+    return Promise.reject("Not your car");
+  }
   bugs.splice(idx, 1);
   return _saveBugs();
 }
 
-function save(bug) {
+function save(bug, loggedInUser) {
   if (bug._id) {
+    const bugToUpdate = bugs.find((currBug) => bug._id === currBug._id);
+    if (!loggedInUser.isAdmin && bugToUpdate.creator._id !== loggedInUser._id) {
+      return Promise.reject("Not your car");
+    }
     const idx = bugs.findIndex((b) => b._id === bug._id);
     if (idx === -1) return Promise.reject("Bug not found");
     bugs[idx] = { ...bugs[idx], ...bug }; // patch
   } else {
     bug._id = makeId();
     bug.createdAt = Date.now();
+    const creator = { _id: loggedInUser._id, fullName: loggedInUser.fullName };
+    bug.creator = creator;
     bugs.push(bug);
   }
   return _saveBugs().then(() => bug);
