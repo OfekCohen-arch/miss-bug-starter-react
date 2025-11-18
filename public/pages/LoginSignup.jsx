ef43b95f2c3aeb6203384235f70ccb08b071d01a
Ofek Cohen <ofek074@gmail.com>
2025-11-18 09:40:03 +0200

    Create <LoginSignup>

--- public/pages/LoginSignup.jsx
+++ public/pages/LoginSignup.jsx
@@ -1,5 +1,51 @@
+const { useState } = React
+const { useNavigate } = ReactRouter
 
-export function LoginSignup(){
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
+import { userService } from '../services/user.service.js'
+import { authService } from '../services/auth.service.js'
+
+export function LoginSignup({setLoggedinUser}){
+    const [isSignup, setIsSignUp] = useState(false)
+    const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
+    
+    const navigate = useNavigate()
+
+    function handleChange({ target }) {
+        const { name: field, value } = target
+        setCredentials(prevCreds => ({ ...prevCreds, [field]: value }))
+    }
+
+    function handleSubmit(ev) {
+        ev.preventDefault()
+        isSignup ? signup(credentials) : login(credentials)
+    }
+
+    function login(credentials) {
+        authService.login(credentials)
+            .then(user => {
+                setLoggedinUser(user)
+                showSuccessMsg('Logged in successfully')
+                navigate('/car')
+            })
+            .catch(err => { 
+                console.log(err)
+                showErrorMsg(`Couldn't login...`) 
+            })
+    }
+
+    function signup(credentials) {
+        authService.signup(credentials)
+            .then(user => {
+                setLoggedinUser(user)
+                showSuccessMsg('Signed in successfully')
+                navigate('/car')
+            })
+            .catch(err => { 
+                console.log(err)
+                showErrorMsg(`Couldn't signup...`) 
+            })
+    }
     return(
         <div className="login-page">
             <form className="login-form" onSubmit={handleSubmit}>
